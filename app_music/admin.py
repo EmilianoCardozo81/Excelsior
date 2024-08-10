2d03e5fe8cd2ce9b7ac28806ca2be8108043b364
Emiliano <cardozoemilianog@gmail.com>
2024-08-10 13:57:13 -0300

    elimine entregables

--- app_music/admin.py
+++ app_music/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Profesor, Estudiante, Curso, Entregable 
+from .models import Profesor, Estudiante, Curso
 
 # Register your models here.
 admin.site.register(Curso)
 admin.site.register(Estudiante)
 admin.site.register(Profesor)
-admin.site.register(Entregable)
