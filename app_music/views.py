d33df15a721fea17e6f46629743b3ad58230be71
Emiliano <cardozoemilianog@gmail.com>
2024-08-14 19:38:21 -0300

    agregue visualización de botones solo a superusuarios

--- app_music/views.py
+++ app_music/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from app_music.models import *
